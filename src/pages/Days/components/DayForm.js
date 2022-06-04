247bca78055517aa6de042ebf7ca07fbe2513cc1
nickrally <nickrally2009@gmail.com>
2022-06-04 08:28:00 -0600

    fixed init date

--- src/pages/Days/components/DayForm.js
+++ src/pages/Days/components/DayForm.js
@@ -3,8 +3,10 @@ import moment from "moment";
 import DayPicker from "./DayPicker";
 import "./DayPicker.css";
 
+const today = new Date().toISOString().split("T")[0];
+
 const DayForm = () => {
-  const [date, setDate] = useState("");
+  const [date, setDate] = useState(today);
   const dayPickerProps = {
     onChange: (date) => {
       setDate(moment(date).toISOString());
